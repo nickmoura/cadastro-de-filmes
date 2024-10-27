cbf234ccb50702f84b2224d4bc0cc80bb0f13360
Nick Moura <nickolas.moura@outlook.com>
2024-10-27 16:44:40 -0300

    tests on js

--- assets/js/script-template.js
+++ assets/js/script-template.js
@@ -50,8 +50,8 @@ function validate(val) {
             flag4 = true;
         }
     }
-    
-    flag = flag1 && flag2 && flag3 && flag4 && flag5 && flag6;
+
+    flag = flag1 && flag2 && flag3 && flag4;
 
     return flag;
 }
